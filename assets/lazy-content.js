eb6295e45a68af50178fd23f2f2120dbc6226d7d
Ivan Chaplinskyi <chaplinskyi.ivan@gmail.com>
2026-01-22 01:43:26 +0200

    lazy content code

--- assets/lazy-content.js
+++ assets/lazy-content.js
@@ -4,19 +4,28 @@ class LazyContent extends HTMLElement {
     }
 
     connectedCallback() {
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    this.loadContent();
-                    observer.unobserve(this);
-                }
-            });
-        }, { rootMargin: '0px 0px 200px 0px' });
+        // const observer = new IntersectionObserver((entries, observer) => {
+        //     entries.forEach(entry => {
+        //         if (entry.isIntersecting) {
+        //             this.loadContent();
+        //             observer.unobserve(this);
+        //         }
+        //     });
+        // }, { rootMargin: '0px 0px 200px 0px' });
 
-        observer.observe(this);
+        // observer.observe(this);
+
+        setTimeout(() => {
+            ['click', 'keydown', 'touchstart', 'mousemove', 'scroll', 'keydown'].forEach(eventType => {
+            document.addEventListener(eventType, () => {
+                this.loadContent();
+            }, { once: true });
+        });
+        }, 1000);
     }
 
     loadContent() {
+        console.log('Loading lazy content');
         const template = this.querySelector('template');
 
         if (!template) return;
